483c913635baa152d2ab266c38f355bfb187c583
Gonçalo Graça Ribeiro <gogasman@gmail.com>
2023-03-29 18:48:08 +0100

    harrambe resurrection!

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const { Client, Events, GatewayIntentBits, Intents} = require('discord.js');
 const { token, nvideaID, tarasManiasID, gandiniFunClubID } = require('./auth.json');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, getVoiceConnection, entersState, demuxProbe, AudioPlayerStatus, VoiceConnectionStatus, StreamType } = require('@discordjs/voice');
-const { exec } = require( 'youtube-dl-exec');
-let guildId = nvideaID;
+// const { exec } = require( 'youtube-dl-exec');
+const ytdl = require('ytdl-core');
+const https = require('https');
 
 var heyClips = require('./heysoundClips.json');
 var soundClips = require('./soundClips.json');
@@ -11,8 +12,21 @@ var soundClips = require('./soundClips.json');
 const player = setUpAudioPlayer();
 let connection = null;
 
+let streamaux
+https.get('https://cdn.discordapp.com/attachments/434883051085103105/1069420247746101268/wassup_baby.mp3', res => streamaux=res);
+
+// const stream1 = createReadStream("https://cdn.discordapp.com/attachments/434883051085103105/1069420247746101268/wassup_baby.mp3")
+// console.log(stream1)
+// const resource1 = createAudioResource(stream1)
+// console.log(resource1)
+// const stream2 = createReadStream("heys\wassup_baby.mp3")
+// console.log(stream2)
+// const resource2 = createAudioResource(stream2)
+// console.log(resource2)
+
 const client = new Client({ intents: 
-    [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages] });
+    [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageReactions] });
 
 // Log in to Discord with your client's token
 client.login(token);
@@ -47,6 +61,37 @@ client.on('ready', function (evt) {
     //registerSlashCommands();
 });
 
+client.on('messageCreate', async (message) => {
+    console.log(streamaux)
+    // console.log(message)
+	console.log(`${message.author} in #${message.channel.name} sent: ${message.content}`);
+	if (!message.guild) return;
+
+	if (message.content.substring(0, 4) === '-hey') {
+        // message.react('🐵');
+		const voiceState = message.member?.voice;
+
+        const args = message.content.substring(4).split(' ');
+        console.log(args)
+
+		if (voiceState && heyClips[args[1]]) {
+			try {
+				// message.reply('Playing '+ heyClips[args[1]].memberName +' user\'s /hey');
+                console.log('Playing '+ heyClips[args[1]].memberName +' user\'s /hey');
+				playHeyClip(args[1], voiceState);
+			} catch (error) {
+				console.error(error);
+			}
+		} else {
+            if(!voiceState){
+                message.reply('Join a voice channel then try again!');
+            }else{
+                message.reply('Please select a valid user ID!');
+            }
+		}
+	}
+});
+
 client.on('voiceStateUpdate', (oldState, newState) =>{
     let userID = oldState.id;
     
@@ -61,9 +106,12 @@ client.on('voiceStateUpdate', (oldState, newState) =>{
 async function playHeyClip(userID, voiceState){   
     console.log('playHeyClip start', userID)
 
-    let link = heyClips[userID].link;
-    let volume = heyClips[userID].volume;
+    const link = heyClips[userID].link;
+    const volume = heyClips[userID].volume;
     const channel = voiceState.channel
+    
+    const youtubeRegex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/
+    const regexResult = link.match(youtubeRegex)
 
     if (channel) {
         try {
@@ -71,28 +119,50 @@ async function playHeyClip(userID, voiceState){
             await connectToChannel(channel);
             console.log('subscribing player...')
             connection.subscribe(player);
-            const stream = getYoutubeStream(link)
+            const stream = regexResult ? ytdl(link, { filter : 'audioonly' }) : await getStreamFromURL(link)
             console.log('Playing audio resource...')
-            playAudioResource(stream)
+            playAudioResource(player, stream, volume)
         } catch (error) {
             console.log(error);
         }
     }       
 }
 
-async function playAudioResource(stream) {
+async function getStreamFromURL(link){
+    console.log('creating Read Stream From URL:', link)
+    return new Promise((resolve, reject) => {
+        const req = https.request(link, (res) => {
+            if (res.statusCode < 200 || res.statusCode >= 300) {
+                return reject(new Error('statusCode=' + res.statusCode));
+            }
+            var body = [];
+            res.on('data', function(chunk) {
+                body.push(chunk);
+            });
+            res.on('end', function() {
+                resolve(body);
+            });
+        });
+        req.on('error', (e) => {
+            reject(e.message);
+        });
+        // send the request
+        req.end();
+    });
+}
+
+async function playAudioResource(player, stream, volume) {
     // console.log(stream)
     try {
-        // const probe = await demuxProbe(stream)
+        const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary, inlineVolume: true });
+        resource.volume.setVolume(volume)
+        // console.log(resource)
 
-        // const resource = createAudioResource(probe.stream, { inputType: probe.type });
-        const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary });
-        
         player.play(resource);
         return entersState(player, AudioPlayerStatus.Playing, 10e3);
     } catch (error) {
         console.log(error)
-        // connection.destroy();
+        connection.destroy();
         throw error;
     }
 }
@@ -114,28 +184,6 @@ async function connectToChannel(channel) {
 	}
 }
 
-function getYoutubeStream(link){
-
-    const process = exec(
-        link,
-        {
-            output: '-',
-            // quiet: true,
-            format: 'ba',
-            // audioFormat: 'mp3',
-            verbose: true,
-            // limitRate: '1M',
-        }
-        ,{ stdio: ['ignore', 'pipe', 'ignore'] },
-    );
-    if (!process.stdout) {
-        console.log('no process.stdout')
-        return;
-    }
-
-    return stream = process.stdout;
-}
-
 function setUpAudioPlayer(){
     let player = createAudioPlayer();
 
@@ -150,7 +198,7 @@ function setUpAudioPlayer(){
 
     // Always remember to handle errors appropriately!
     player.on('error', error => {
-        console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
+        console.error(`Error: ${error.message} with resource ${error.resource}`);
     });
     return player;
 
